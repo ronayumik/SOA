8842a234c070d150cd06405064682edd17be16eb
Didit Sepiyanto <sepiyantodidit@gmail.com>
2016-04-16 10:57:32 +0700

    Pembuatan Fitur dosen masih di lihat jadwal

---
+++ application/controllers/TU.php
@@ -0,0 +1,19 @@
+ <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TU extends CI_Controller {
+   
+	public function index()
+	{
+		$this->load->view('tu/index');
+	}
+	
+	public function mengelola_akun_dosen()
+	{
+        
+    }
+	public function mengelola_pengumuman()
+	{
+       
+	}
+}
