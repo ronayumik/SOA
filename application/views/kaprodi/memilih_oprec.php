998255528ced8837502149ebe412d7edcd7bdb56
ibnukipa <ibnu.prayogi44@gmail.com>
2016-05-09 09:17:56 +0700

    mark is done was gone

--- application/views/kaprodi/memilih_oprec.php
+++ application/views/kaprodi/memilih_oprec.php
@@ -94,9 +94,9 @@
                         <i class="material-icons icon-list-oprec no-back" style="color: white; font-size: 20px; padding: 1px;">mode_edit</i>
                         <span style="line-height: 0; font-size: 15px; vertical-align: middle">Jadwal</span>
                       </button>
-                      <button onclick="mark_done('<?php echo $oprec->j_id; ?>')" style="margin-right: 10px; color: black; float: right;" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-shadow--2dp">
+                      <!-- <button onclick="mark_done('<?php echo $oprec->j_id; ?>')" style="margin-right: 10px; color: black; float: right;" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-shadow--2dp">
                         Mark as done
-                      </button>
+                      </button> -->
                     <?php } else { ?>
                       <button style="margin-right: 8px; color: #B6B6B6; float: right; " class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent ">
                         <i class="material-icons icon-list-oprec no-back" style="color: #B6B6B6; font-size: 20px; padding: 1px;">mode_edit</i>
