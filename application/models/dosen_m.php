f8073f302d357a993cfdc764fe5c641c22bb652e
Andre <setiawan.andre03@gmail.com>
2016-12-14 22:30:13 +0700

    password hashed

--- application/models/dosen_m.php
+++ application/models/dosen_m.php
@@ -1,13 +1,13 @@
 <?php
 Class Dosen_m extends CI_Model{
-    
+
     public function __construct()
     {
         parent::__construct();
         $this->load->database();
     }
-    
-	
+
+
     public function list_kelas($id_dosen, $id_jadwal) {
         $data = $this->db->query("  SELECT *
                                     FROM kelas AS k , asisten AS a, list_mata_kuliah AS list_mk, user_ AS dosen
@@ -27,14 +27,14 @@ Class Dosen_m extends CI_Model{
         $data = $query->result();
         return $data;
     }
-    
+
     public function list_daftar_asisten($id_dosen)
     {
         $query = $this->db->query("select * from asisten, asisten_daftar, kelas, list_mata_kuliah where k_id_dosen = $id_dosen and ad_id_kelas = k_id_kelas and a_nrp = ad_nrp_mhs and lmk_id = k_matkul order by lmk_nama ASC, k_kelas ASC, ad_ipk DESC, ad_nilai_matkul ASC");
         $data = $query->result();
         return $data;
     }
-    
+
     public function pilih_asisten($id_kelas, $nrp)
     {
         $query = $this->db->query("update asisten_daftar set ad_keterangan = 'DITERIMA' where ad_id_kelas = '$id_kelas' and ad_nrp_mhs = '$nrp'");
@@ -46,19 +46,19 @@ Class Dosen_m extends CI_Model{
     {
         $query = $this->db->query("update user_ set u_nama = '$nama', u_email='$email' where u_nip = '$id'");
     }
-    
+
     public function edit_pass($id, $baru)
     {
         $query = $this->db->query("update user_ set u_pass = '$baru' where u_nip = '$id'");
     }
-    
+
     public function detail_dosen($id)
     {
         $query = $this->db->query("select * from user_ where u_nip = '$id'");
         $data = $query->result();
         return $data;
     }
-    
+
     public function login_d($email, $pass)
     {
         //echo "$email - $pass";
@@ -66,17 +66,18 @@ Class Dosen_m extends CI_Model{
         $data = $query->result();
         return $data;
     }
-    
-    
-    
+
+
+
     public function login($email, $pass)
     {
+        $hashed_pass = md5($pass);
         //echo "$email - $pass";
-        $query = $this->db->query("SELECT * FROM user_ WHERE u_email = '$email' AND u_pass = '$pass'");
+        $query = $this->db->query("SELECT * FROM user_ WHERE u_email = '$email' AND u_pass = '$hashed_pass'");
         $data = $query->result();
         return $data;
     }
-    
+
 }
-    
+
 ?>
